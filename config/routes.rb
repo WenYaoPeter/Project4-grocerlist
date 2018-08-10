6af7f4742e5b24b94b204f5e0fce020df1efd813
Lee Wen Yao <peterleewenyao@gmail.com>
2018-08-10 10:05:16 +0800

    setup database, controller, model

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :categories do
+    resources :items
+  end
+  
+  resources :items do
+    resources :categories
+  end
+
+end
