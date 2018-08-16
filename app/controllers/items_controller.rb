9b52e1a3876b881873a26650c7c89a7564fb2bcf
Lee Wen Yao <peterleewenyao@gmail.com>
2018-08-16 14:18:34 +0800

    redirect - home, delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,41 +20,41 @@ class ItemsController < ApplicationController
       itemToGet = Item.create(item_params)
       itemToGet.save
       currentCat = Category.find(params[:category_id])
-      currentCat.items << itemToGet
-      # if length > 0
-      redirect_to new_category_item_path
-
-      # else
-      #    redirect_to same page
-
-
-
+      currentCat.items << itemToGet     
+      allCurrentItems = currentCat.items.map do |item|
+         item
+      end
+      redirect_to edit_category_item_path(currentCat, allCurrentItems)
    end
-
    
    def edit
-      @items = Item.find(params[:id])
+      # @items = Item.find(params[:id])
       @categories = Category.find(params[:category_id])
       @categoryItems = @categories.items
+   end
 
+   def destroy
+      itemToDestroy = Item.find(params[:id])
+      if itemToDestroy.destroy
+         # categories id
+         currentCat = Category.find(params[:category_id])
+         # categories.items id
+         currentItems = currentCat.items.map do |item|
+            item
+         end
+         redirect_to edit_category_item_path(currentCat, currentItems)
+      end
    end
    
-
-
+   
    def update
       itemToUpdate = Item.find(params[:id])
       itemToUpdate.update(item_params)
       category = Category.find(params[:category_id])
       redirect_to edit_category_item_path
    end
-
    
-   def destroy
-      itemToDestroy = Item.find(params[:id])
-      itemToDestroy.destroy
-      
-      redirect_to new_category_item_path
-   end
+   
 
    
 
